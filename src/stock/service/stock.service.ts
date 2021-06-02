119e7b6378f6f5f6e747897db508fa2c1ccfd73e
Martin Brodersen <martinroager@gmail.com>
2021-06-02 02:05:00 +0200

    .

---
+++ src/stock/service/stock.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Stock } from '../shared/stock.model';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class StockService {
+  public stocks: Stock[] = [];
+
+  createStock(stock: Stock): Stock {
+    // const id = uuidv4()
+    if (stock.name.length < 2) {
+      throw new Error('Stock name must be more then 2 chars');
+    }
+    stock.id = uuidv4();
+    this.stocks.push(stock);
+    return stock;
+  }
+}
